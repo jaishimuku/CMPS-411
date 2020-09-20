117074f74b3a3b8085dad296b81998e78886fd6c
CG <chandani.gurung@selu.edu>
2020-09-20 12:54:57 -0500

    delete

--- SLU.CS.LAB/Data/DataContext.cs
+++ SLU.CS.LAB/Data/DataContext.cs
@@ -1,14 +1,28 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using SLU.CS.LAB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace SLU.CS.LAB.Data
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        public DataContext(DbContextOptions<DataContext> options)
+          : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Message>()
+                .HasOne<AppUser>(a => a.AppUser)
+                .WithMany(d => d.Messages)
+                .HasForeignKey(d => d.UserID);
 
         }
-        public DbSet<User> Users { get; set; }
     }
 }
---
+++ SLU.CS.LAB/Models/Message.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SLU.CS.LAB.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Text { get; set; }
+        public DateTime When { get; set; }
+
+        public int UserID { get; set; }
+        public virtual AppUser AppUser { get; set; }
+
+
+
+    }
+}
