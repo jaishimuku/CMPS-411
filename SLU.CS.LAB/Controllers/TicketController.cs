7b5142ba5013c89bd9f053acc8be4a9c66b98998
Louis Gurung <louis.gurung@selu.edu>
2020-10-17 20:04:41 -0500

    Backend for Ticket

---
+++ SLU.CS.LAB/Controllers/TicketController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SLU.CS.LAB.Data;
+using SLU.CS.LAB.Models;
+
+namespace SLU.CS.LAB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketController : Controller
+    {
+        private readonly DataContext _context;
+        public TicketController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public ActionResult CreateTicket(Ticket ticket)
+        {
+            if (ModelState.IsValid)
+            {
+                var newTicket = new Ticket
+                {
+                    Title = ticket.Title,
+                    Description = ticket.Description,
+                    IsResolved = false,
+                    SubmittedBy = ticket.SubmittedBy
+                };
+                _context.Add(newTicket);
+                _context.SaveChanges();
+
+                return Ok();
+            }
+            else {
+                return StatusCode(400);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult GetAllTickets() {
+
+            var tickets = _context.Tickets.ToList();
+
+            return Ok(tickets);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult MarkTicketResolved(int id) {
+
+            var ticket = _context.Tickets.Where(x => x.Id == id).FirstOrDefault();
+            if (ticket == null)
+            {
+                return StatusCode(400);
+            }
+            else {
+                ticket.IsResolved = true;
+                _context.SaveChanges();
+                return Ok(ticket);
+            }
+        }
+    }
+}
---
+++ SLU.CS.LAB/Migrations/20201016221451_added model ticket.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SLU.CS.LAB.Migrations
+{
+    public partial class addedmodelticket : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tickets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    IsResolved = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tickets", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tickets");
+        }
+    }
+}
