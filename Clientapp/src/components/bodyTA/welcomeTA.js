7ffefe5af91714cafb0a14ff45bb0ab4ba604b24
rsapkota123 <roshan.sapkota@selu.edu>
2020-10-19 10:21:20 -0500

    dashboard basics

--- Clientapp/src/components/bodyTA/welcomeTA.js
+++ Clientapp/src/components/bodyTA/welcomeTA.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {
   makeStyles,
   Card,
   Typography,
   CardContent,
-  Grid,
 } from "@material-ui/core";
+import GroupIcon from '@material-ui/icons/Group';
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBCardHeader,
+  MDBBtn,
+  MDBInput
+} from "mdbreact";
 import Container from "@material-ui/core/Container";
 import { ToastContainer, toast } from "react-toastify";
 import { connect, useDispatch } from "react-redux";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css'
 
 import LayoutTA from "../../Layout/SidebarTA/indexTA";
 import { loginMsg } from "../../module/actions";
+import baseURL from "../../baseURL";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -39,15 +53,52 @@ const useStyles = makeStyles((theme) => ({
     height: "100%",
     overflow: "auto",
   },
+  paper: {
+    marginTop:20,
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
   title: {
     textAlign: "center",
     margin: 15,
   },
+  contents: {
+    padding: '5px 10px',
+    marginLeft: 120,
+    marginTop: 30,
+    height: 60
+  },
+  number: {
+    display: 'block',
+     fontWeight: "medium",
+    fontSize: 18,
+    color: "grey"
+  },
+  text: {
+    fontSize: 20,
+     fontWeight: 'normal',
+    color: "black"
+  },
+  iconSpan: {
+    float: 'left',
+    height: 90,
+    width: 90,
+    textAlign: 'center',
+    backgroundColor: "white"
+  },
+  icon: {
+    height: 40,
+    width: 40,
+    maxWidth: '100%'
+
+  }
 }));
 
 const WelcomeTA = (props) => {
   const classes = useStyles();
   const dispatch = useDispatch();
+  const[tas, setTa] = useState([]);
 
   const hideMsg = () => {
     dispatch(loginMsg(false));
@@ -62,6 +113,17 @@ const WelcomeTA = (props) => {
     draggable: true,
     progress: undefined,
   };
+
+  const fetchTA = () => {
+    fetch(`${baseURL}/api/Schedule/current`)
+      .then((response) => response.json())
+      .then((res) => setTa(res))
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    fetchTA();
+  });
 
   return (
     <div>
@@ -86,6 +148,33 @@ const WelcomeTA = (props) => {
                   </Typography>
                 </CardContent>
               </Card>
+              <MDBContainer style={{marginRight:20}}>
+            <MDBRow>
+                <MDBCol xs="5">
+
+                    <MDBCard style={{backgroundColor:'white', marginTop:90}}>
+                        <MDBCardHeader className="form-header font-italic rounded " style={{marginLeft:20, marginTop:-25, marginRight:20, backgroundColor: "#ffa500" }}>
+                            <h3 className="my-3 text-left text-white">
+                                Currently Working TAS
+                            </h3>
+                        </MDBCardHeader>
+                        <MDBCardBody>
+                        {/* <Paper> */}
+        <div className={classes.iconSpan}>
+          <GroupIcon color="primary"
+                className={classes.icon}
+          />
+          <span className={classes.text}>{tas.taToReturn}</span>
+        </div>
+        <div className={classes.contents}>
+          <span className={classes.number}>{tas.timeToReturn}</span>
+        </div>
+      {/* </Paper> */}
+                        </MDBCardBody>
+                    </MDBCard>
+                </MDBCol>
+            </MDBRow>
+        </MDBContainer>
             </Container>
           </div>
         </div>
